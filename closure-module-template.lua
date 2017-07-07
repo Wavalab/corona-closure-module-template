2f27a580a04ad41bce5126c6905cc07e37b7b495
John Chin-Jew <jchinjew@gmail.com>
2017-07-06 23:16:34 -0700

    Update closure-module-template.lua

--- closure-module-template.lua
+++ closure-module-template.lua
@@ -1,34 +1,31 @@
-local M = { new = function()
+return {
+  new = function(params)
 
-  -- Construct and initialize instance
-  local ClosureInstance = {} -- feel free to rename to something more elegant
+    -- Construct and initialize instance
+    local ClosureInstance = display.newCircle(0, 0, 0) or {}
+    -- local ClosureInstance = require("parent-class-module").new() -- To extend a custom object
 
-  -- To extend a display object
-  -- local ClosureInstance = display.newCircle(0, 0, 0)
+    -- Public properties
+    ClosureInstance.publicVariable = nil
 
-  -- To extend a custom object
-  -- local ClosureInstance = require("parent-class-module").new()
+    -- Private variables
+    local privateVariable
 
-  -- Public properties
-  ClosureInstance.publicVariable = nil
+    -- Private functions
+    local function privateFunction() end
 
-  -- Private variables
-  local privateVariable
+    -- Public methods with access to `self` reference
+    function ClosureInstance:publicFunction() end
 
-  -- Private functions
-  local function privateFunction() end
+    -- Overriding inherited methods
+    -- function ClosureInstance:inheritedMethod() end
 
-  -- Public methods with access to `self` reference
-  function ClosureInstance:publicFunction() end
+    -- Calling super class methods
+    -- ClosureInstance.superInheritedMethod = ClosureInstance.inheritedMethod
+    -- function ClosureInstance:inheritedMethod()
+    --   self:superInheritedMethod()
+    --   -- extend method...
+    -- end
 
-  -- Override inherited methods
-  -- function ClosureInstance:inheritedMethod() end
-
-  -- Call super class methods
-  -- ClosureInstance.superInheritedMethod = ClosureInstance.inheritedMethod
-  -- function ClosureInstance:inheritedMethod()
-  --   self:superInheritedMethod()
-  --   -- extend method...
-  -- end
-
-return ClosureInstance end } return M
+  return ClosureInstance end
+}
